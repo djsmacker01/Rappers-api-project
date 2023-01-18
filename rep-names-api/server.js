7df12e2f3e75ad3954154500d1bdbbd609a25ccc
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-18 06:23:52 +0000

    Main.js

--- rep-names-api/server.js
+++ rep-names-api/server.js
@@ -1,51 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5000;
 
 const rappers = {
-  "Rick Ross": {
-    age: 46,
-    birthName: "William Leonard Roberts II",
-    birthLocation: "Clarksdale, Mississippi, U.S.",
-    
+
+  "rick ross": {
+    "age": 46,
+    "birthName": "William Leonard Roberts II",
+    "birthLocation": "Clarksdale, Mississippi, U.S.",
   },
 
   "Chance the Rapper": {
-    age: 29,
-    birthName: "Chancelor Johnathan Bennett ",
-    birthLocation: "Chicago,Illinois",
+    "age": 29,
+    "birthName": " Johnathan Bennett ",
+    "birthLocation": "Chicago,Illinois",
   },
 
-  "Eminem": {
-    age: 39,
-    birthName: "Chancelor Johnathan Bennett ",
-    birthLocation: "St. Joseph, Missouri, U.S.",
+  "eminem": {
+    "age": 40,
+    "birthName": " Johnathan Bennett ",
+    "birthLocation": "St. Joseph, Missouri, U.S.",
   },
 
-   "Dylan": {
-    age: 39,
-    birthName: "Chancelor  ",
-    birthLocation: " U.S.",
+  "dylan": {
+   " age": 39,
+    "birthName": "dylan  ",
+    "birthLocation": " U.S.",
   }
-
-  
 };
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html');
-})
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
 
-app.get('/api/:rapperName', (request, response) => {
-  const rappersName = request.params.rapperName
-  if(rappers[rappersName]){
-    response.json(rappers[rappersName])
+app.get("/api/:rapperName", (request, response) => {
+  const rappersName = request.params.rapperName.toLowerCase();
+  if (rappers[rappersName]) {
+    response.json(rappers[rappersName]);
+  } else {
+    response.json(rappers["dylan"]);
   }
-  else{
-    response.json(rappers['Dylan'])
-  }
-    // response.json(rappers)
+  // response.json(rappers)
 });
 
 app.listen(PORT, () => {
-    console.log(`The server is listening on  port ${PORT}`);
-})
+  console.log(`The server is listening on  port ${PORT}`);
+});
