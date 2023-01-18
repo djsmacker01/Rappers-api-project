1a88e6070c789b6b66a39d11ff48597464e528e9
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-01-18 20:21:50 +0000

    modified client side

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api-fun.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://simple-rapper-api.herokuapp.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
